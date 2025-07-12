c6f67c85a51e9d7e59a56593a8ce64e22fe6d069
fibs <etienne.muser@hotmail.com>
2025-07-12 11:29:55 +0200

    Fix message formatting

--- Infrastructure/OutputAdapters/DiscordStatusMessageSender.cs
+++ Infrastructure/OutputAdapters/DiscordStatusMessageSender.cs
@@ -9,6 +9,8 @@ namespace Infrastructure.OutputAdapters;
 
 public class DiscordStatusMessageSender(DiscordSocketClient client, IConfiguration config) : IStatusMessageSender
 {
+    private const int MaxNumPlayersPerMessage = 15;
+    
     public async Task SendActivityStatusUpdateMessageAsync(List<GeoGuessrClubMemberActivityStatus> statuses)
     {
         // Get the server
@@ -29,37 +31,65 @@ public class DiscordStatusMessageSender(DiscordSocketClient client, IConfigurati
             throw new InvalidOperationException($"No channel found for id {_channelId}");
         }
 
-        // Build the message
-        var messageString = _buildStatusUpdateMessageString(statuses);
-
-        // Send the message
-        await channel.SendMessageAsync(messageString).ConfigureAwait(false);
-    }
-
-    private string _buildStatusUpdateMessageString(List<GeoGuessrClubMemberActivityStatus> statuses)
-    {
-        // Create a string builder
-        var builder = new StringBuilder("**======= Activity status update =======**\n\n");
-
-        // Add header for members that failed to meet the requirement
-        builder.Append("----- Members that failed to meet the ");
-        builder.Append(_requirement);
-        builder.Append("XP requirement -----");
-
         // Get the players that failed to meet the requirement
         var playersWithFailedRequirement = statuses
             .Where(s => !s.TargetAchieved)
             .OrderByDescending(s => s.NumStrikes)
             .ToList();
+        
+        // Build the message first part of the message
+        var messageString = _buildStatusUpdateMessageBeginningString(playersWithFailedRequirement.Take(MaxNumPlayersPerMessage).ToList());
+
+        // Send the message
+        await channel.SendMessageAsync(messageString).ConfigureAwait(false);
+
+        // While there are still players to be processed
+        var skipCount = MaxNumPlayersPerMessage;
+        while (playersWithFailedRequirement.Skip(skipCount).Any())
+        {
+            // Create a string builder
+            var builder = new StringBuilder();
+            
+            // Get the players for this chunk
+            var playersChunk = playersWithFailedRequirement.Skip(skipCount).Take(MaxNumPlayersPerMessage).ToList();
+            
+            // Append the players to the string builder
+            _appendPlayers(builder, playersChunk);
+            
+            // Send the message
+            await channel.SendMessageAsync(builder.ToString()).ConfigureAwait(false);
+            
+            // Increase skip count
+            skipCount += MaxNumPlayersPerMessage;
+        }
+    }
+
+    private string _buildStatusUpdateMessageBeginningString(List<GeoGuessrClubMemberActivityStatus> players)
+    {
+        // Create a string builder
+        var builder = new StringBuilder("**======= Activity status update =======**\n\n");
+
+        // Add header for members that failed to meet the requirement
+        builder.Append("Members that failed to meet the ");
+        builder.Append(_requirement);
+        builder.Append("XP requirement:");
 
         // If no player failed the requirement
-        if (playersWithFailedRequirement.Count == 0)
+        if (!players.Any())
         {
             builder.Append("\n```diff\n+ None :)\n```");
         }
 
+        // Append the players
+        _appendPlayers(builder, players);
+        
+        return builder.ToString();
+    }
+    
+    private void _appendPlayers(StringBuilder builder, List<GeoGuessrClubMemberActivityStatus> players)
+    {
         // For every player that failed the requirement
-        foreach (var player in playersWithFailedRequirement)
+        foreach (var player in players)
         {
             // Add a new line
             builder.AppendLine();
@@ -68,7 +98,7 @@ public class DiscordStatusMessageSender(DiscordSocketClient client, IConfigurati
             if (player.IsOutOfStrikes)
             {
                 // Append player in red text
-                builder.Append("* ```diff\n- ");
+                builder.Append("```diff\n- ");
                 builder.Append(player.Nickname);
                 builder.Append(" got only ");
                 builder.Append(player.XpSinceLastUpdate);
@@ -88,8 +118,6 @@ public class DiscordStatusMessageSender(DiscordSocketClient client, IConfigurati
                 builder.Append(" strikes.");
             }
         }
-
-        return builder.ToString();
     }
 
     private readonly ulong _guildId = config.GetValue<ulong>(ConfigKeys.ActivityCheckerMainServerIdConfigurationKey);
