d01ee88772a33adde2684b36ab0491bdcb9a4779
fibs <etienne.muser@hotmail.com>
2025-07-12 21:43:58 +0200

    Make the activity module require admin permissions

--- Infrastructure/InputAdapters/Commands/ActivityModule.cs
+++ Infrastructure/InputAdapters/Commands/ActivityModule.cs
@@ -1,7 +1,9 @@
+using Discord;
 using Discord.Interactions;
 
 namespace Infrastructure.InputAdapters.Commands;
 
+[DefaultMemberPermissions(GuildPermission.Administrator)]
 [Group("member-activity", "Commands for interacting with the club member activity checker")]
 public partial class ActivityModule : InteractionModuleBase<SocketInteractionContext>
 {
