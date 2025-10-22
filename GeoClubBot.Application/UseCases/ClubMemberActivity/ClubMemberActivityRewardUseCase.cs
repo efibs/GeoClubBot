63884f2bc67f15172688f4b16eac7ef6da7bb236
fibs <etienne.muser@hotmail.com>
2025-10-22 16:20:25 +0200

    Also remove MVP role again, if member is no longer MVP

--- GeoClubBot.Application/UseCases/ClubMemberActivity/ClubMemberActivityRewardUseCase.cs
+++ GeoClubBot.Application/UseCases/ClubMemberActivity/ClubMemberActivityRewardUseCase.cs
@@ -27,52 +27,86 @@ public class ClubMemberActivityRewardUseCase(IGeoGuessrUserIdsToDiscordUserIdsUs
             .Take(3)
             .ToList();
         
-        // If there are no players to mention
-        if (leaderboard.Count == 0)
-        {
-            return;
-        }
+        // Send the mention
+        var mvpPlayerUserIds = await _mentionMvpsAsync(leaderboard).ConfigureAwait(false);
         
-        // The builder for the message
-        var msgBuilder = new StringBuilder("# The new MVP's of the club are here! :partying_face:\n");
-        msgBuilder.AppendLine("The club members that achieved the most club XP since last time are:");
+        // Update the roles
+        await _updateRolesAsync(mvpPlayerUserIds).ConfigureAwait(false);
+    }
 
-        var place = 1;
+    private async Task<List<string>> _mentionMvpsAsync(List<IGrouping<int, ClubMemberActivityStatus>> leaderboard)
+    {
         IEnumerable<string> mvpPlayerUserIds = [];
-        foreach (var group in leaderboard)
+        
+        try
         {
-            msgBuilder.Append("**#");
-            msgBuilder.Append(place);
-            msgBuilder.Append(":** With ");
-            msgBuilder.Append(group.Key);
-            msgBuilder.Append("XP: ");
-            msgBuilder.Append(string.Join(", ", group.Select(s => s.Nickname)));
-
-            if (place == 1)
+            // If there are no players to mention
+            if (leaderboard.Count == 0)
             {
-                msgBuilder.Append(group.Count() > 1 
-                    ? " (Our new MVP's) :clap: " 
-                    : " (Our new MVP) :clap: ");
-                
-                mvpPlayerUserIds = group.Select(s => s.UserId);
+                return [];
             }
-            
-            msgBuilder.AppendLine();
-            
-            place++;
+
+            // The builder for the message
+            var msgBuilder = new StringBuilder("# The new MVP's of the club are here! :partying_face:\n");
+            msgBuilder.AppendLine("The club members that achieved the most club XP since last time are:");
+
+            var place = 1;
+            foreach (var group in leaderboard)
+            {
+                msgBuilder.Append("**#");
+                msgBuilder.Append(place);
+                msgBuilder.Append(":** With ");
+                msgBuilder.Append(group.Key);
+                msgBuilder.Append("XP: ");
+                msgBuilder.Append(string.Join(", ", group.Select(s => s.Nickname)));
+
+                if (place == 1)
+                {
+                    msgBuilder.Append(group.Count() > 1
+                        ? " (Our new MVP's) :clap: "
+                        : " (Our new MVP) :clap: ");
+
+                    mvpPlayerUserIds = group.Select(s => s.UserId);
+                }
+
+                msgBuilder.AppendLine();
+
+                place++;
+            }
+
+            // Build the message
+            var msg = msgBuilder.ToString();
+
+            // Send the message
+            await messageAccess.SendMessageAsync(msg, _channelId).ConfigureAwait(false);
         }
-        
-        // Build the message
-        var msg = msgBuilder.ToString();
-        
-        // Send the message
-        await messageAccess.SendMessageAsync(msg, _channelId).ConfigureAwait(false);
-        
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to mention MVPs.");
+        }
+
+        return mvpPlayerUserIds.ToList();
+    }
+
+    private async Task _updateRolesAsync(IEnumerable<string> mvpPlayerUserIds)
+    {
         // Get the discord user ids for the mvps
         var discordUserIds = await geoGuessrUserIdsToDiscordUserIdsUseCase.GetDiscordUserIdsAsync(mvpPlayerUserIds).ConfigureAwait(false);
         
+        // Get the members that already have the mvp role
+        var membersWithMvpRole = await serverRolesAccess.ReadMembersWithRoleAsync(_mvpRoleId).ConfigureAwait(false);
+        
+        // Get the members that need the mvp role
+        var membersToAddMvpRole = discordUserIds.Except(membersWithMvpRole);
+        
+        // Get the members that are no longer mvp
+        var membersToRemoveMvpRole = membersWithMvpRole.Except(discordUserIds);
+        
         // Distribute the roles
-        await serverRolesAccess.AddRoleToMembersByUserIdsAsync(discordUserIds, _mvpRoleId).ConfigureAwait(false);
+        await serverRolesAccess.AddRoleToMembersByUserIdsAsync(membersToAddMvpRole, _mvpRoleId).ConfigureAwait(false);
+        
+        // Remove the roles
+        await serverRolesAccess.RemoveRoleFromPlayersAsync(membersToRemoveMvpRole, _mvpRoleId).ConfigureAwait(false);
     }
     
     private readonly string _channelId = config.GetValue<string>(ConfigKeys.ActivityRewardTextChannelIdConfigurationKey)!;
