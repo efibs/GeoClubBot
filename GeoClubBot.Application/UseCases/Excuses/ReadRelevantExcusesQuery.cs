b121370b0e7ec9ba26cd472accd17903ed404a76
fibs <etienne.muser@hotmail.com>
2026-06-11 22:56:29 +0200

    Add previous excuses to relevant excuses

--- GeoClubBot.Application/UseCases/Excuses/ReadRelevantExcusesQuery.cs
+++ GeoClubBot.Application/UseCases/Excuses/ReadRelevantExcusesQuery.cs
@@ -6,14 +6,15 @@ using Utilities;
 
 namespace UseCases.UseCases.Excuses;
 
-public sealed record ReadRelevantExcusesQuery(int UpcomingExcusesNumDays) : IQuery<Result<List<ClubMemberRelevantExcuse>>>;
+public sealed record ReadRelevantExcusesQuery(int UpcomingExcusesNumDays, DateTimeOffset? LastActivityCheckTime = null)
+    : IQuery<Result<List<ClubMemberRelevantExcuse>>>;
 
 public sealed class ReadRelevantExcusesHandler(IExcusesRepository repo) : IRequestHandler<ReadRelevantExcusesQuery, Result<List<ClubMemberRelevantExcuse>>>
 {
     public async Task<Result<List<ClubMemberRelevantExcuse>>> Handle(ReadRelevantExcusesQuery request, CancellationToken cancellationToken)
     {
         var excuses = await repo
-            .ReadAllRelevantExcusesAsync(request.UpcomingExcusesNumDays, cancellationToken)
+            .ReadAllRelevantExcusesAsync(request.UpcomingExcusesNumDays, request.LastActivityCheckTime, cancellationToken)
             .ConfigureAwait(false);
         return Result<List<ClubMemberRelevantExcuse>>.Success(excuses);
     }
--- GeoClubBot.Domain/ClubMemberRelevantExcuse.cs
+++ GeoClubBot.Domain/ClubMemberRelevantExcuse.cs
@@ -2,10 +2,12 @@ using Utilities;
 
 namespace Entities;
 
-public record ClubMemberRelevantExcuse(string MemberNickname, TimeRange ExcuseTimeRange, bool IsUpcoming)
+public record ClubMemberRelevantExcuse(string MemberNickname, TimeRange ExcuseTimeRange, bool IsUpcoming, bool IsPrevious = false)
 {
     public override string ToString()
     {
+        if (IsPrevious)
+            return $"{MemberNickname} ({ExcuseTimeRange.ToString("yyyy-MM-dd")})";
         return IsUpcoming
             ? $"{MemberNickname} ({ExcuseTimeRange.ToString("yyyy-MM-dd")})"
             : $"{MemberNickname} until {ExcuseTimeRange.To:yyyy-MM-dd}";
